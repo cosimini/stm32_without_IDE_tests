f0c89917681a9b2db63b9577f5d867dbb1769b08
cosimini <davide.scalcon@studenti.unipd.it>
2025-03-05 21:56:56 +0000

    Ok now the reset vector is really correctly initialized
    Well, not really, considering that I'm initializing some reserved space to 0

--- boot.c
+++ boot.c
@@ -8,56 +8,57 @@ const uint32_t RAM_END = 0x20002000;
 extern int main(void); // Forward declare the main()
 
 // Useful functions to fill the reset vector
-void trap_loop(void) { while(1); }
-void nop(void) {}
+void trap(void) { while(1); }  // Trap the execution indefietely
+void noop(void) {}             // Do nothing
 
+// The vector table
 uint32_t __attribute__((section(".reset_vector"))) isr_vector[] = {
-/*  0 */ (uint32_t) RAM_END,    // Initial SP value
-/*  1 */ (uint32_t) main,       // Reset handler
-/*  2 */ (uint32_t) nop,        // NMI
-/*  3 */ (uint32_t) trap_loop,  // Hard fault handler
-/*  4 */ (uint32_t) 0,          // reserved
-/*  5 */ (uint32_t) 0,          // reserved
-/*  6 */ (uint32_t) 0,          // reserved
-/*  7 */ (uint32_t) 0,          // reserved
-/*  8 */ (uint32_t) 0,          // reserved
-/*  9 */ (uint32_t) 0,          // reserved
-/* 10 */ (uint32_t) 0,          // reserved
-/* 11 */ (uint32_t) nop,        // SVCall
-/* 12 */ (uint32_t) 0,          // reserved
-/* 13 */ (uint32_t) 0,          // reserved
-/* 14 */ (uint32_t) nop,        // PendSV
-/* 15 */ (uint32_t) nop,        // SysTick
-/* 17 */ // IRQs
-/* 16 */ (uint32_t) nop,
-/* 18 */ (uint32_t) nop,
-/* 19 */ (uint32_t) nop,
-/* 20 */ (uint32_t) nop,
-/* 21 */ (uint32_t) nop,
-/* 22 */ (uint32_t) nop,
-/* 23 */ (uint32_t) nop,
-/* 24 */ (uint32_t) nop,
-/* 25 */ (uint32_t) nop,
-/* 26 */ (uint32_t) nop,
-/* 27 */ (uint32_t) nop,
-/* 28 */ (uint32_t) nop,
-/* 29 */ (uint32_t) nop,
-/* 30 */ (uint32_t) nop,
-/* 31 */ (uint32_t) nop,
-/* 32 */ (uint32_t) nop,
-/* 33 */ (uint32_t) nop,
-/* 34 */ (uint32_t) nop,
-/* 35 */ (uint32_t) nop,
-/* 36 */ (uint32_t) nop,
-/* 37 */ (uint32_t) nop,
-/* 38 */ (uint32_t) nop,
-/* 39 */ (uint32_t) nop,
-/* 40 */ (uint32_t) nop,
-/* 41 */ (uint32_t) nop,
-/* 42 */ (uint32_t) nop,
-/* 43 */ (uint32_t) nop,
-/* 44 */ (uint32_t) nop,
-/* 45 */ (uint32_t) nop,
-/* 46 */ (uint32_t) nop,
-/* 47 */ (uint32_t) nop,
+  (uint32_t) RAM_END,  //  0 - Initial SP value
+  (uint32_t) main,     //  1 - Reset handler
+  (uint32_t) noop,     //  2 - NMI
+  (uint32_t) trap,     //  3 - Hard fault handler
+  (uint32_t) 0,        //  4 - reserved
+  (uint32_t) 0,        //  5 - reserved
+  (uint32_t) 0,        //  6 - reserved
+  (uint32_t) 0,        //  7 - reserved
+  (uint32_t) 0,        //  8 - reserved
+  (uint32_t) 0,        //  9 - reserved
+  (uint32_t) 0,        // 10 - reserved
+  (uint32_t) noop,     // 11 - SVCall
+  (uint32_t) 0,        // 12 - reserved
+  (uint32_t) 0,        // 13 - reserved
+  (uint32_t) noop,     // 14 - PendSV
+  (uint32_t) noop,     // 15 - SysTick
+  (uint32_t) noop,     // 16 - IRQ 0
+  (uint32_t) noop,     // 17 - IRQ 1
+  (uint32_t) noop,     // 18 - IRQ 2
+  (uint32_t) noop,     // 19 - IRQ 3
+  (uint32_t) noop,     // 20 - IRQ 4
+  (uint32_t) noop,     // 21 - IRQ 5
+  (uint32_t) noop,     // 22 - IRQ 6
+  (uint32_t) noop,     // 23 - IRQ 7
+  (uint32_t) noop,     // 24 - IRQ 8
+  (uint32_t) noop,     // 25 - IRQ 9
+  (uint32_t) noop,     // 26 - IRQ 10
+  (uint32_t) noop,     // 27 - IRQ 11
+  (uint32_t) noop,     // 28 - IRQ 12
+  (uint32_t) noop,     // 29 - IRQ 13
+  (uint32_t) noop,     // 30 - IRQ 14
+  (uint32_t) noop,     // 31 - IRQ 15
+  (uint32_t) noop,     // 32 - IRQ 16
+  (uint32_t) noop,     // 33 - IRQ 17
+  (uint32_t) noop,     // 34 - IRQ 18
+  (uint32_t) noop,     // 35 - IRQ 19
+  (uint32_t) noop,     // 36 - IRQ 20
+  (uint32_t) noop,     // 37 - IRQ 21
+  (uint32_t) noop,     // 38 - IRQ 22
+  (uint32_t) noop,     // 39 - IRQ 23
+  (uint32_t) noop,     // 40 - IRQ 24
+  (uint32_t) noop,     // 41 - IRQ 25
+  (uint32_t) noop,     // 42 - IRQ 26
+  (uint32_t) noop,     // 43 - IRQ 27
+  (uint32_t) noop,     // 44 - IRQ 28
+  (uint32_t) noop,     // 45 - IRQ 29
+  (uint32_t) noop,     // 46 - IRQ 30
+  (uint32_t) noop,     // 47 - IRQ 31
 };
