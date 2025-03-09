116d8ac2fa5a4549df38d005fcfd970c008b76df
cosimini <davide.scalcon@studenti.unipd.it>
2025-03-09 20:23:58 +0000

    Using macro functions makes it look way better

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdint.h>
 
+// GPIO stuff
+#define GPIO_OFFSET 0x50000000
+#define GPIOC_OFFSET 0x800
+#define GPIO_MODESET 0x000
+#define GPIO_TYPESET 0x004
+#define GPIO_PULLSET 0x00C
+#define GPIO_OUT_SET 0x014
+
+#define SET_BIT_32(reg, idx, val) *reg = (*reg & ~((uint32_t) 0b01 << idx)) | ((uint32_t) val << idx)
+#define SET_PAIR_32(reg, idx, val_0, val_1) *reg = (*reg & ~((uint32_t) 0b11 << (2 * idx)) | ((uint32_t) (val_0 | (val_1 << 1)) << (2 * idx)))
+
 int main(void) {
   // Dummy execution that might be useful
   // for(int k = 0; k < 100; k++) {}
@@ -9,29 +20,31 @@ int main(void) {
   // MODE : 01
   // OTYPE : 0
   // PUPD  : 0 0 
-  volatile uint32_t* clock_reg = (volatile uint32_t*) 0x40021034;
-  volatile uint32_t* moder_reg = (volatile uint32_t*) 0x50000800;
-  volatile uint32_t* otype_reg = (volatile uint32_t*) 0x50000804;
-  volatile uint32_t* pupdr_reg = (volatile uint32_t*) 0x5000080C;
-  volatile uint32_t* outpt_reg = (volatile uint32_t*) 0x50000814;
 
-  volatile uint32_t value = 0;
+  // TODO: Make it easyer to address this
+  uint32_t* clock_reg = (uint32_t*) 0x40021034;
+  //GPIO_OFFSET + GPIOC_OFFSET + GPIO_MODESET
+  uint32_t* moder_reg = (uint32_t*) 0x50000800;
+  //GPIO_OFFSET + GPIOC_OFFSET + GPIO_TYPESET
+  uint32_t* otype_reg = (uint32_t*) 0x50000804;
+  //GPIO_OFFSET + GPIOC_OFFSET + GPIO_PULLSET
+  uint32_t* pupdr_reg = (uint32_t*) 0x5000080C;
+  //GPIO_OFFSET + GPIOC_OFFSET + GPIO_OUT_SET
+  uint32_t* outpt_reg = (uint32_t*) 0x50000814;
+
+
+  uint32_t value = 0;
 
   // Enable clocks
-  value = *clock_reg;
-  *clock_reg = value | 0b00000000000000000000000000111111;
+  SET_BIT_32(clock_reg, 2, 1);
   // set mode
-  value = *moder_reg & 0b11111111111111111100111111111111;
-  *moder_reg = value | 0b00000000000000000001000000000000;
+  SET_PAIR_32(moder_reg, 6, 1, 0);
   // set type
-  value = *otype_reg & 0b11111111111111111111111110111111;
-  *otype_reg = value | 0b00000000000000000000000000000000;
+  SET_BIT_32(otype_reg, 6, 0);
   // set pull up/down
-  value = *pupdr_reg & 0b11111111111111111100111111111111;
-  *pupdr_reg = value | 0b00000000000000000000000000000000;
+  SET_PAIR_32(pupdr_reg, 6, 0, 0);
   // Turn on
-  value = *outpt_reg & 0b11111111111111111111111110111111;
-  *outpt_reg = value | 0b00000000000000000000000001000000;
+  SET_BIT_32(outpt_reg, 6, 1);
 
   // Infinite loop
   while(1) { }
